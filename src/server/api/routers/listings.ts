466b427530f0864ba8ff9ed0f816f14a1c03732e
Folka2134 <folka2134@gmail.com>
2023-06-30 10:12:05 +0100

    listings are now displayed on home page

--- src/server/api/routers/listings.ts
+++ src/server/api/routers/listings.ts
@@ -2,6 +2,9 @@ import { z } from "zod";
 import { createTRPCRouter, protectedProcedure, publicProcedure } from "~/server/api/trpc";
 
 export const listingsRouter = createTRPCRouter({
+  list: publicProcedure.query(({ctx}) => {
+    return ctx.prisma.listing.findMany()
+  }),
   create: protectedProcedure
   .input(
     z.object({ name: z.string(), description: z.string(), price: z.number() })

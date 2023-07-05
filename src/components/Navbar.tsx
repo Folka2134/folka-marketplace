137b251c4059c997918eec44e5387184beeb2481
Folka2134 <folka2134@gmail.com>
2023-07-05 10:51:56 +0100

    not signed in users won't be able to see messages link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -59,15 +59,17 @@ export function Navbar() {
                 Sell item
               </Link>
             </li>
-            <li>
-              <Link
-                href="/messages"
-                className="block rounded bg-blue-700 py-2 pl-3 pr-4 text-white dark:text-white md:bg-transparent md:p-0 md:text-blue-700 md:dark:text-blue-500"
-                aria-current="page"
-              >
-                Messages
-              </Link>
-            </li>
+            {user.isSignedIn && (
+              <li>
+                <Link
+                  href="/messages"
+                  className="block rounded bg-blue-700 py-2 pl-3 pr-4 text-white dark:text-white md:bg-transparent md:p-0 md:text-blue-700 md:dark:text-blue-500"
+                  aria-current="page"
+                >
+                  Messages
+                </Link>
+              </li>
+            )}
             <li>
               {user.isSignedIn ? (
                 <UserButton afterSignOutUrl="/" />
